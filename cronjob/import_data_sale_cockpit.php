94fd5efaa8fc20496630d9c4ad4f207dcf3638f9
beckpalmx77 <beckpalmx@gmail.com>
2025-10-03 18:31:10 +0700

    Update import_data_sale_cockpit.php

--- cronjob/import_data_sale_cockpit.php
+++ cronjob/import_data_sale_cockpit.php
@@ -63,8 +63,9 @@ $in_clause = "'" . implode("', '", $all_docs) . "'";
 // 3. สร้างเงื่อนไข SQL ทั้งหมด
 $query_daily_cond_ext = " AND (DOCTYPE.DT_DOCCODE in (" . $in_clause . ")) ";
 
-$query_year = " AND DI_DATE BETWEEN '2017/01/01' AND '2023/07/31'";
-//$query_year = " AND DI_DATE BETWEEN '2025/10/01' AND '" . date("Y/m/d") . "'";
+//$query_year = " AND DI_DATE BETWEEN '2017/01/01' AND '2023/07/31'";
+
+$query_year = " AND DI_DATE BETWEEN '2025/01/01' AND '" . date("Y/m/d") . "'";
 
 //$query_year = " AND DI_DATE BETWEEN '" . date("Y/m/d", strtotime("yesterday")) . "' AND '" . date("Y/m/d") . "'";
 
